9c50a7ce5fc589667636a1cd7b1fc11e3f9a3b97
Eduardo <eduar766@gmail.com>
2019-04-29 14:11:20 -0400

    implementing basic search post

--- src/blog/urls.py
+++ src/blog/urls.py
@@ -3,13 +3,14 @@ from django.conf.urls.static import static
 
 from django.contrib import admin
 from django.urls import path
-from post.views import index, blog, post
+from post.views import index, blog, post, search
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog, name='post-list'),
     path('post/<id>/', post, name='postsito'),
+    path('search/', search, name='search'),
 ]
 
 if settings.DEBUG:
--- src/post/views.py
+++ src/post/views.py
@@ -2,7 +2,24 @@ from django.shortcuts import render
 from .models import Post
 from marketing.models import Signup
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-from django.db.models import Count
+from django.db.models import Count, Q
+
+
+def search(request):
+    queryset = Post.objects.all()
+    query = request.GET.get('q')
+    if query:
+        queryset = queryset.filter(
+            Q(title__icontains=query) |
+            Q(overview__icontains=query)
+        ).distinct() # distincts its used for return only one valor, even if repeat.
+    
+    context = {
+        'queryset': queryset
+    }
+
+    return render(request, 'search_results.html', context)
+
 
 
 def get_category_count():
